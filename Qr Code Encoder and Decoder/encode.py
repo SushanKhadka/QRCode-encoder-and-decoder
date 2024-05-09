a98c6f5c5aba9c5026a19faa0f3dd2b1f9c2b2af
Sushan Khadka <sushan.khadka404@gmail.com>
2024-05-10 00:07:16 +0545

    Merging remote repo with main repo

---
+++ Qr Code Encoder and Decoder/encode.py
@@ -0,0 +1,15 @@
+import qrcode
+
+qr = qrcode.QRCode(
+    version = 15,
+    error_correction = qrcode.constants.ERROR_CORRECT_H,
+    box_size = 5,
+    border = 4
+)
+
+def encode(data):
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    return qr.make_image(fill_color = "black", back_color="white")
+
